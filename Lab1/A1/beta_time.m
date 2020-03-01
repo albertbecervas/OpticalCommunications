517d816e15a929c8abb946303265d1f61ad9172f
Adrián Marín <adriii6556@gmail.com>
2020-03-01 22:29:29 +0100

    Sections A1-1 and A1-2 revised. Added section A1-3.

--- Lab1/A1/beta_time.m
+++ Lab1/A1/beta_time.m
@@ -1,3 +1,3 @@
 function [beta] = beta_time(sigma_in,sigma_out,L)
-beta = sqrt((sigma_out^2 - sigma_in^2)*sigma_in^2)/L;
+beta = sqrt((sigma_out^2 - sigma_in^2)*sigma_in^2)/ L;
 end
--- Lab1/A1/dispersion.m
+++ Lab1/A1/dispersion.m
@@ -1,7 +1,7 @@
 function [D] = dispersion(lambda,beta)
 %DISPERSION function to calculate a fiber dispersion parameter
 %   D = -(2pi * c)/Lambda * ß
-c = physconst('LightSpeed');
+c       =   physconst('LightSpeed') / 1e3;      % Light velocity (nm/ps).
 D = -(2*pi*c/lambda)*beta;
 end
 
